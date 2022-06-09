2d2f87e5c89d5c15d8fcfbeaaf286102f8497ffc
RobertoCuevasP <pablorobertocp1@gmail.com>
2022-06-09 16:40:31 -0400

    Backend initial structure

---
+++ backend/app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers", 
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({
+        message: 'Post added successfully'
+    })
+});
+
+app.get("/api/posts",(req, res, next) => {
+    const posts = [
+        {
+            id: "1234",
+            title: "First post",
+            description: "This is the description",
+            image: "Something.png"
+        },
+        {
+            id: "1235",
+            title: "Second post",
+            description: "This is the description",
+            image: "Something2.png"
+        }
+    ];
+    res.status(200).json({
+        message: 'Posts fetched succesfully',
+        posts: posts
+    })
+});
+
+
+module.exports = app;
