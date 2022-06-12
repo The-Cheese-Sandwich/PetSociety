96dd947e0d740fac5b8adf9585e1bba1f0dcd7ad
Raisa Alejnadra Silva Plata <raisa2201silva@gmail.com>
2022-06-12 18:13:39 -0400

    Login validation hide and show eye

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeDashComponent } from './home-dash/home-dash.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [];
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  // variable - default false
+show: boolean = false;
+
+constructor() {
+}
+
+// click event function toggle
+
+  password() {
+    this.show = !this.show;
+  }  
 
   ngOnInit(): void {
+  }
+
+  changeState(id : string){
+    var list = document.getElementsByClassName('selected');
+    if(list.length > 0) {      
+      list[0].classList.toggle('deselect');
+      list[0].classList.toggle('selected');
+    } 
+    document.getElementById(id)?.classList.toggle('deselect'); 
+    document.getElementById(id)?.classList.toggle('selected');
   }
 
 }
