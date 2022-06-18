9ea487de8e28a6b72fd2e53a13ca70df3482683e
LizVasquez <lizvasmen@gmail.com>
2022-06-18 19:06:01 -0400

    delete and add functions on backend

--- backend/app.js
+++ backend/app.js
@@ -35,11 +35,12 @@ app.post("/api/posts", (req, res, next) => {
         image: req.body.image,
         description: req.body.description
     });
-    post.save();
-    //console.log(post);
-    res.status(201).json({
-        message: 'Post added successfully'
-    })
+    post.save().then(createdPost => {
+        res.status(201).json({
+            message: 'Post added successfully',
+            id: createdPost._id
+        });
+    });
 });
 
 app.get("/api/posts",(req, res, next) => {
@@ -50,6 +51,15 @@ app.get("/api/posts",(req, res, next) => {
         });
    });
 });
+
+app.delete("api/posts/:id", checkAuth, (req, res, next) => {
+    Post.deleteOne({
+      _id: req.params.id
+    }).then(result => {
+      console.log(result);
+      res.status(200).json({ message: "Post deleted!"});
+    })
+  });
 
 
 module.exports = app;
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Subject } from "rxjs";
+import { map } from "rxjs/operators";
+
 
 import { Post } from "../models/post.model";
 
@@ -13,14 +15,25 @@ export class PostsService {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>(
+      .get<{ message: string; posts: any }>(
         "http://localhost:3000/api/posts"
       )
-      .subscribe(postData => {
-        this.posts = postData.posts;
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            id: post._id,
+            image: post.image,
+            description: post.description
+            
+          };
+        });
+      }))
+      .subscribe(transformedPosts => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
+
 
   getPostUpdateListener() {
     return this.postsUpdated.asObservable();
@@ -29,11 +42,22 @@ export class PostsService {
   addPost(image: string, description: string) {
     const post: Post = { id: '', image: image, description: description };
     this.http
-      .post<{ message: string }>("http://localhost:3000/api/posts", post)
+      .post<{ message: string, id: string }>("http://localhost:3000/api/posts", post)
       .subscribe(responseData => {
-        console.log(responseData.message);
+        const id = responseData.id;
+        post.id = id;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
       });
+  }
+
+
+  deletePost(id:string){
+    this.http.delete("http://localhost:3000/api/posts/" + id)
+    .subscribe(() => {
+      const updatedPosts = this.posts.filter(post => post.id !== id);
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 }
