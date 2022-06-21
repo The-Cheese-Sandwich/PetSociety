b37f8712792dd4e0eb8251ea39c617263a60769f
luvellana <luciananunezl99@gmail.com>
2022-06-21 16:53:41 -0400

    nuevo component post de usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeDashComponent } from './home-dash/home-dash.component';
 import { PublicacionesGeneralComponent } from './publicaciones-general/publicaciones-general.component';
-import { GuardadosComponent } from './guardados/guardados.component';
 import { MisPublicacionesComponent } from './mis-publicaciones/mis-publicaciones.component';
 import { AuthGuard } from './guards/auth.guard';
 
@@ -17,7 +16,6 @@ const routes: Routes = [
         children: [
           { path: '', redirectTo: 'publicaciones', pathMatch: 'full'},
           { path: "publicaciones", component: PublicacionesGeneralComponent },
-          { path: "guardados", component: GuardadosComponent },
           { path: "mis-publicaciones", component: MisPublicacionesComponent }
         ]
       }
---
+++ src/app/publicacion-usuario/publicacion-usuario.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-publicacion-usuario',
+  templateUrl: './publicacion-usuario.component.html',
+  styleUrls: ['./publicacion-usuario.component.css']
+})
+export class PublicacionUsuarioComponent implements OnInit {
+
+  @Input() publicacion?: any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
